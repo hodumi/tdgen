588cf531408797c1cca22aebc28d48d9d47677e5
hodumi <ajsmithy00@gmail.com>
2016-03-05 18:37:41 +0900

    CLIをsub-command形式に変更

---
+++ bin/tdgen-make.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+'use strict';
+
+var tdgen = require('../tdgen.js');
+var program = require('commander');
+var path = require('path');
+
+program
+    .version("0.0.1")
+    .usage('<formatFile> <dataFile>')
+    .parse(process.argv);
+
+
+if (program.args.length >= 2) {
+    var formatFile = path.resolve(program.args[0]);
+    var dataFile = path.resolve(program.args[1]);
+
+    tdgen.generate(formatFile, dataFile);
+} else {
+    program.help();
+}
--- bin/tdgen.js
+++ bin/tdgen.js
@@ -1,24 +1,9 @@
 #!/usr/bin/env node
 'use strict';
 
-var tdgen = require('../tdgen.js');
 var program = require('commander');
-var path = require('path');
-
 
 program
     .version('0.0.1')
-    .usage('<template> <data>')
+    .command('make <formatFile> <dataFile>', 'generate file from formatFile and dataFile')
     .parse(process.argv);
-
-
-if (program.args.length == 2) {
-    // run generate file
-
-    var formatFile = path.resolve(program.args[0]);
-    var dataFile = path.resolve(program.args[1]);
-
-    tdgen.generate(formatFile, dataFile);
-} else {
-    program.help();
-}
